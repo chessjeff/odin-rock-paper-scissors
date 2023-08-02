366135b9ff7a4595b37f0c9689a205332f0acdd8
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-08-02 14:44:35 -0500

    Write playRound function

--- game.js
+++ game.js
@@ -4,22 +4,67 @@
 
 // assign random number (1 - 3) to "rock" "paper" "scissors"
 function getComputerChoice(max=3) { 
-    let randInt = Math.floor(Math.random() * max + 1);
+    const randInt = Math.floor(Math.random() * max + 1);
     let computerChoice;
 
     switch(randInt) {
         case 1:
-            computerChoice = "Rock"
+            computerChoice = "rock"
             break;
         case 2:
-            computerChoice = "Paper"
+            computerChoice = "paper"
             break;
         default:
-            computerChoice = "Scissors"
+            computerChoice = "scissors"
     }
 
     return computerChoice;
 }
+
+// plays a round of rock paper scissors
+// 
+
+function playRound(playerSelection, computerSelection) { // manually insert playerSelection, call computerSelection()
+    let result;
+    let lowerPlayerSelection = playerSelection.toLowerCase()
+    
+    switch(lowerPlayerSelection) {
+        case "rock":
+            if(computerSelection === "paper") {
+                result = `You lose! ${computerSelection} beats Rock!`
+            } else if (computerSelection === "scissors") {
+                result = `You win!! Rock beats ${computerSelection}`
+            } else {
+                result = "It's a draw!"
+            }
+            break;
+        case "paper":
+            if(computerSelection === "scissors") {
+                result = `You lose! ${computerSelection} beats Paper!`
+            } else if (computerSelection === "rock") {
+                result = `You win!! Paper beats ${computerSelection}`
+            } else {
+                result = "It's a draw!"
+            }
+            break;
+        case "scissors":
+            if(computerSelection === "rock") {
+                result = `You lose! ${computerSelection} beats Scissors!`
+            } else if (computerSelection === "paper") {
+                result = `You win!! Scissors beats ${computerSelection}`
+            } else {
+                result = "It's a draw!"
+            }
+            break;
+    }
+
+    return result;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection))
 
 // compare user input to random: 
 //      rock > scissors 
