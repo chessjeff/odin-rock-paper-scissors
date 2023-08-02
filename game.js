17641cc0774a309e22ec230802829558bfc94aef
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-08-02 13:15:48 -0500

    Write js pseudo code

---
+++ game.js
@@ -0,0 +1,9 @@
+// receive a string from player
+// string must === "rock", "paper", or "scissors"
+// assign random number (1 - 3) to "rock" "paper" "scissors"
+// compare user input to random: 
+//      rock > scissors 
+//      paper > rock
+//      scissors > paper
+// if user input wins: "congrats, you win"
+//      if computer wins: "i win"
