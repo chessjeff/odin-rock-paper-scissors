9897cb44f12148d5f31ac578c7123a6c4ddaa310
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-08-02 13:54:38 -0500

    Write a getComputerChoice function

--- game.js
+++ game.js
@@ -1,6 +1,26 @@
 // receive a string from player
 // string must === "rock", "paper", or "scissors"
+
+
 // assign random number (1 - 3) to "rock" "paper" "scissors"
+function getComputerChoice(max=3) { 
+    let randInt = Math.floor(Math.random() * max + 1);
+    let computerChoice;
+
+    switch(randInt) {
+        case 1:
+            computerChoice = "Rock"
+            break;
+        case 2:
+            computerChoice = "Paper"
+            break;
+        default:
+            computerChoice = "Scissors"
+    }
+
+    return computerChoice;
+}
+
 // compare user input to random: 
 //      rock > scissors 
 //      paper > rock
