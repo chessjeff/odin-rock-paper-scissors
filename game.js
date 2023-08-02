13d2e96aceca11af73364bafcf770f01a268faa1
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-08-02 17:10:50 -0500

    begin work on game() function

--- game.js
+++ game.js
@@ -21,13 +21,17 @@ function getComputerChoice(max=3) {
     return computerChoice;
 }
 
-// plays a round of rock paper scissors
-// 
-
+// plays a round of rock paper scissors and returns game result
 function playRound(playerSelection, computerSelection) { // manually insert playerSelection, call computerSelection()
     let result;
     let lowerPlayerSelection = playerSelection.toLowerCase()
     
+    // compare user input to random: 
+    // rock > scissors 
+    // paper > rock
+    // scissors > paper
+    // if user input wins: "congrats, you win"
+    // if computer wins: "i win"
     switch(lowerPlayerSelection) {
         case "rock":
             if(computerSelection === "paper") {
@@ -42,7 +46,7 @@ function playRound(playerSelection, computerSelection) { // manually insert play
             if(computerSelection === "scissors") {
                 result = `You lose! ${computerSelection} beats Paper!`
             } else if (computerSelection === "rock") {
-                result = `You win!! Paper beats ${computerSelection}`
+                result = `You win!! Paper beats ${computerSelection}!`
             } else {
                 result = "It's a draw!"
             }
@@ -51,24 +55,43 @@ function playRound(playerSelection, computerSelection) { // manually insert play
             if(computerSelection === "rock") {
                 result = `You lose! ${computerSelection} beats Scissors!`
             } else if (computerSelection === "paper") {
-                result = `You win!! Scissors beats ${computerSelection}`
+                result = `You win!! Scissors beats ${computerSelection}!`
             } else {
                 result = "It's a draw!"
             }
             break;
     }
-
+    console.log(result);
     return result;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+//const playerSelection = "paper";
+//const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection))
+//console.log(playRound(playerSelection, computerSelection))
 
-// compare user input to random: 
-//      rock > scissors 
-//      paper > rock
-//      scissors > paper
-// if user input wins: "congrats, you win"
-//      if computer wins: "i win"
+function game() {
+// run the game 5 times
+// prompt player for input for each round
+// keep a tally of wins and losses
+// return overall winner after 5 rounds
+
+    let playerTally = 0;
+    let computerTally = 0;
+    let roundScore; //string
+
+    // one instance of rock paper scissors
+    // run game 5 times while keeping score
+    for (let i = 0; i < 5; i++) {
+        // get input
+        let playerSelection = prompt("Enter rock, paper, or scissors", "")
+        // get computerChoice
+        let roundChoice = getComputerChoice()
+        // run game
+        let roundResult = playRound(playerSelection, roundChoice)
+
+        // trying to create a score counter. detect "win" or "lose" from string given by playRound()?        
+    } 
+}
+
+// function that capitalizes the first letter of computer input for print statement
