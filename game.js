15dee29f675a52c31d3279470d5421419109300b
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-09-26 16:01:54 -0500

    Clears the screen at each button press

--- game.js
+++ game.js
@@ -8,6 +8,12 @@ buttons.forEach((button) => {
 let playerScore = 0;
 let computerScore = 0;
 
+const scoreContainer = document.querySelector('#scoreContainer');
+const score = document.createElement('div');
+
+const resultList = document.querySelector('#result');
+const content = document.createElement('div');
+
 // assign random number (1 - 3) to "rock" "paper" "scissors"
 function getComputerChoice(max=3) { 
     const randInt = Math.floor(Math.random() * max + 1);
@@ -32,6 +38,9 @@ function playRound(playerSelection) {
     let result;
     let computerSelection = getComputerChoice();
     
+    //resultList.removeChild(content);
+    //scoreContainer.removeChild(score);
+
     switch(playerSelection) {
         case "rock":
             if(computerSelection === "paper") {
@@ -73,29 +82,13 @@ function playRound(playerSelection) {
 }
 
 function printGameResult(playRoundResult) {
-    const resultList = document.querySelector('#result');
-    const content = document.createElement('div');
     content.textContent = playRoundResult;
     resultList.appendChild(content);
 } 
 
 function printScore(player, computer) {
-    const scoreContainer = document.querySelector('#scoreContainer');
-    const score = document.createElement('div');
     score.textContent = `Human ${player} Computer ${computer}`;
     scoreContainer.appendChild(score);
 }
 
 
-
-
-//playRound needs to take just one parameter (the button press)
-//playRound calls getComputerChoice
-//playRound compares these two and returns result
-
-//printGameResult() takes playRound Return 
-//creates a div for the result of the game
-
-//printScore() counts divs in container
-//
-//
